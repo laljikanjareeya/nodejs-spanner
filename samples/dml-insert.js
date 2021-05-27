4d7e150da58e0c63416b50338d585eeac8e316e7
Lalji Kanjareeya <lalji.kanjareeya@qlogic.io>
2021-05-27 19:02:22 +0530

    fix: fix lint

--- samples/dml-insert.js
+++ samples/dml-insert.js
@@ -51,8 +51,7 @@ function main(
       }
       try {
         const [rowCount] = await transaction.runUpdate({
-          sql:
-            'INSERT Singers (SingerId, FirstName, LastName) VALUES (10, @firstName, @lastName)',
+          sql: 'INSERT Singers (SingerId, FirstName, LastName) VALUES (10, @firstName, @lastName)',
           params: {
             firstName: 'Virginia',
             lastName: 'Watson',
